22e375f1c1be8c413a452b8ad7061a107ddb255c
hemraj-007 <hemrajbhatia38@gmail.com>
2024-09-16 23:00:21 +0530

    initialised prisma db and created some routes

---
+++ app/api/users/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+import {prisma} from '@/prisma/client'
+
+export async function GET(req:NextRequest) {
+    const users=await prisma.user.findMany();
+    return NextResponse.json(users);
+}
+
+export async function Post(req:NextRequest) {
+    const body=await req.json();
+    const validation=schema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, {
+          status: 400,
+        });
+        const user = await prisma.user.findUnique({
+            where: { email: body.email },
+          });
+        
+          if (user)
+            return NextResponse.json(
+              { error: "User already exists" },
+              { status: 400 }
+            );
+        
+        const newUser=prisma.user.create({
+            data:{
+                name:body.name,
+                email:body.email,
+            }
+        })
+        return NextResponse.json(newUser, { status: 201 });
+}
